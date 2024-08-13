745efa53398250abdac5bbee38452a6028e15508
Josiren <egor.novikov666@bk.ru>
2024-08-14 02:46:23 +0300

    change address

--- src/app/ui/footer/ui/contacts.tsx
+++ src/app/ui/footer/ui/contacts.tsx
@@ -7,7 +7,7 @@ export default function Contacts() {
                 <h3 className="text-white text-[16px] font-bold">Контакты</h3>
                 <div className="text-[14px] mt-[8.5px] flex flex-col gap-[14px]">
                     <p className="text-[14px] text-white">Если речь идет об автомобилях, давайте поговорим.</p>
-                    <p className="text-[14px] text-[#DFDFDF]">пр. Ленинского Комсомола, 15, Курск, 305026</p>
+                    <p className="text-[14px] text-[#DFDFDF]">ул. Полянская, 20, Курск, Курская обл., 305026</p>
                 </div>
             </div>
             <div className="flex flex-col gap-2.5 text-[14px] text-right text-white">
