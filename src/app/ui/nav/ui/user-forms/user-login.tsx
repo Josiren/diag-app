9ab81e4d026c140e9b79d1adf877aba0dea26176
Josiren <egor.novikov666@bk.ru>
2024-08-14 22:05:07 +0300

    fixes user-forms validation

--- src/app/ui/nav/ui/user-forms/user-login.tsx
+++ src/app/ui/nav/ui/user-forms/user-login.tsx
@@ -1,21 +1,16 @@
-import React from "react";
-import {
-    Modal,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Button,
-    Checkbox,
-} from "@nextui-org/react";
+import React, { useRef } from "react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Checkbox } from "@nextui-org/react";
 import { MailIcon } from "@icons/header/user-forms/MailIcon";
 import { LockIcon } from "@icons/header/user-forms/LockIcon";
 import Link from "next/link";
 import { Input } from "@nextui-org/input";
 
 export default function UserLogin({ isOpen, onClose }) {
+    // State for email and password
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
 
     const validateEmail = (value) =>
         value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
@@ -26,11 +21,35 @@ export default function UserLogin({ isOpen, onClose }) {
         );
 
     const isInvalid = React.useMemo(() => {
-        if (email === "" || password === "") return false;
-
+        if (email === "" || password === "") return true;
         return !validateEmail(email) || !validatePassword(password);
     }, [email, password]);
 
+    const handleKeyDown = (event, nextField) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            if (nextField) {
+                nextField.current.focus();
+            } else {
+                handleSubmit();
+            }
+        }
+    };
+
+
+    // Handle form submission
+    const handleSubmit = () => {
+        if (email === "" || password === "") {
+            console.log("Please fill in all fields.");
+            return;
+        }
+
+        if (!isInvalid) {
+            console.log("Logging in with:", { email, password });
+            onClose();
+        }
+
+    };
 
     return (
         <Modal
@@ -45,25 +64,39 @@ export default function UserLogin({ isOpen, onClose }) {
                 <ModalBody>
                     <Input
                         autoFocus
+                        ref={emailRef}
                         endContent={<MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />}
                         placeholder="Введите вашу почту"
                         value={email}
                         type="email"
                         label="Email"
+                        errorMessage={
+                            <span className="mt-[4px]">
+                                Пожалуйста, введите действительный адрес электронной почты
+                            </span>
+                        }
                         isInvalid={!validateEmail(email) && email !== ""}
-                        errorMessage="Please enter a valid email"
                         onValueChange={setEmail}
+                        onKeyDown={(event) => handleKeyDown(event, passwordRef)}
                     />
+
                     <Input
+                        ref={passwordRef}
                         endContent={<LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />}
                         label="Password"
                         placeholder="Введите ваш пароль"
                         type="password"
                         value={password}
-                        onValueChange={setPassword}
+                        errorMessage={
+                            <span className="mt-[4px]">
+                                 Пароль должен состоять не менее чем из 8 символов и содержать как минимум одну строчную букву, одну заглавную букву, одну цифру и один специальный символ.
+                            </span>
+                        }
                         isInvalid={!validatePassword(password) && password !== ""}
-                        errorMessage="Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character."
+                        onValueChange={setPassword}
+                        onKeyDown={(event) => handleKeyDown(event)}
                     />
+
                     <div className="flex py-2 px-1 justify-between">
                         <Checkbox classNames={{ label: "text-small" }}>
                             Запомнить меня
@@ -84,7 +117,7 @@ export default function UserLogin({ isOpen, onClose }) {
                     <Button
                         className="rounded-[10px] bg-[#1F2937] text-white"
                         variant="flat"
-                        onPress={onClose}
+                        onPress={handleSubmit}
                         disabled={isInvalid}
                     >
                         Войти
--- src/app/ui/nav/ui/user-forms/user-register.tsx
+++ src/app/ui/nav/ui/user-forms/user-register.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button,} from "@nextui-org/react";
+import React, { useRef } from "react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button } from "@nextui-org/react";
 import { MailIcon } from "@icons/header/user-forms/MailIcon";
 import { LockIcon } from "@icons/header/user-forms/LockIcon";
 import { Input } from "@nextui-org/input";
@@ -8,6 +8,8 @@ import Link from "next/link";
 export default function UserRegister({ isOpen, onClose }) {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
 
     const validateEmail = (value) =>
         value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
@@ -18,10 +20,32 @@ export default function UserRegister({ isOpen, onClose }) {
         );
 
     const isInvalid = React.useMemo(() => {
-        if (email === "" || password === "") return false;
-
+        if (email === "" || password === "") return true;
         return !validateEmail(email) || !validatePassword(password);
     }, [email, password]);
+
+    const handleKeyDown = (event, nextField) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            if (nextField) {
+                nextField.current.focus();
+            } else {
+                handleSubmit();
+            }
+        }
+    };
+
+    const handleSubmit = () => {
+        if (email === "" || password === "") {
+            console.log("Please fill in all fields.");
+            return;
+        }
+
+        if (!isInvalid) {
+            console.log("Registering with:", { email, password });
+            onClose();
+        }
+    };
 
     return (
         <Modal
@@ -36,16 +60,24 @@ export default function UserRegister({ isOpen, onClose }) {
                 <ModalBody>
                     <Input
                         autoFocus
+                        ref={emailRef}
                         endContent={<MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />}
                         placeholder="Введите вашу почту"
                         value={email}
                         type="email"
                         label="Email"
                         isInvalid={!validateEmail(email) && email !== ""}
-                        errorMessage="Please enter a valid email"
+                        errorMessage={
+                            <span className="mt-[4px]">
+                                Пожалуйста, введите действительный адрес электронной почты
+                            </span>
+                        }
+
                         onValueChange={setEmail}
+                        onKeyDown={(event) => handleKeyDown(event, passwordRef)}
                     />
                     <Input
+                        ref={passwordRef}
                         endContent={<LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />}
                         label="Password"
                         placeholder="Введите ваш пароль"
@@ -53,7 +85,12 @@ export default function UserRegister({ isOpen, onClose }) {
                         value={password}
                         onValueChange={setPassword}
                         isInvalid={!validatePassword(password) && password !== ""}
-                        errorMessage="Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character."
+                        errorMessage={
+                            <span className="mt-[4px]">
+                                 Пароль должен состоять не менее чем из 8 символов и содержать как минимум одну строчную букву, одну заглавную букву, одну цифру и один специальный символ.
+                            </span>
+                        }
+                        onKeyDown={(event) => handleKeyDown(event)}
                     />
                     <Link className="text-[14px] ml-[5px]" href="#">
                         У вас уже есть аккаунт?
@@ -69,7 +106,8 @@ export default function UserRegister({ isOpen, onClose }) {
                     </Button>
                     <Button
                         className="rounded-[10px] bg-[#1F2937] text-white"
-                        onPress={onClose}
+                        variant="flat"
+                        onPress={handleSubmit}
                         disabled={isInvalid}
                     >
                         Зарегистрироваться
